df906824dd6814e73d95f82b858a8f2628ce5971
Oxshady <shadymahmod55@gmail.com>
2024-08-04 05:36:05 +0300

    get obj db storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,8 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """return obj based on id"""
+        return self.__session.query(cls).get(id)
+    
