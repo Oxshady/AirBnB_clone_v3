154234c1f664a3dcc1dcfc651c2e0a49125a9791
Oxshady <shadymahmod55@gmail.com>
2024-08-06 02:34:53 +0300

    CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,8 +11,10 @@ from api.v1.views import app_views
 from os import getenv
 from flask_cors import CORS
 
+
 app = Flask(__name__)
 CORS(app=app, resources={r"/*": {'origins':'0.0.0.0'}})
+
 
 @app_views.errorhandler(werkzeug.exceptions.NotFound)
 def page_not_found(error):
