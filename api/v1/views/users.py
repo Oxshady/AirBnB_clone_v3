e37dd402ec50507ca136bba7fea7efca8a9ecf15
Oxshady <shadymahmod55@gmail.com>
2024-08-06 01:36:11 +0300

    users ^_^

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""
-users endpoint
-"""
+"""users endpoint"""
+
 from api.v1.views import app_views
 from flask import request, jsonify, abort
 from models import storage
@@ -16,7 +15,9 @@ def get_users():
     return jsonify(data)
 
 
-@app_views.route("/users/<user_id>", methods=['GET'], strict_slashes=False)
+@app_views.route(
+    "/users/<user_id>",
+    methods=['GET'], strict_slashes=False)
 def get_user(user_id):
     """Get specific user by user id endpoint"""
     obj = storage.get(User, user_id)
@@ -25,7 +26,9 @@ def get_user(user_id):
     return jsonify(obj.to_dict())
 
 
-@app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route(
+    '/users/<user_id>',
+    methods=['DELETE'], strict_slashes=False)
 def delete_user(user_id):
     """Delete specific user by user id endpoint"""
     obj = storage.get(User, user_id)
@@ -36,7 +39,9 @@ def delete_user(user_id):
     return jsonify({}), 200
 
 
-@app_views.route("/users", methods=['POST'], strict_slashes=False)
+@app_views.route(
+    "/users", methods=['POST'],
+    strict_slashes=False)
 def create_user():
     """Add new user endpoint"""
     data = request.get_json()
@@ -44,15 +49,15 @@ def create_user():
         abort(400, 'Not a JSON')
     if 'name' not in data:
         abort(400, 'Missing name')
-    if 'email' not in data:
-        abort(400, 'Missing email')
     obj = User(**data)
     storage.new(obj)
     storage.save()
     return jsonify(obj.to_dict()), 201
 
 
-@app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route(
+    '/users/<user_id>',
+    methods=['PUT'], strict_slashes=False)
 def update_user(user_id):
     """Update user object"""
     obj = storage.get(User, user_id)
@@ -62,7 +67,7 @@ def update_user(user_id):
     if not data:
         abort(400, 'Not a JSON')
     for key, value in data.items():
-        if key not in ['id', 'email', 'created_at', 'updated_at']:
+        if key not in ['id', 'created_at', 'updated_at']:
             setattr(obj, key, value)
     storage.save()
     return jsonify(obj.to_dict()), 200
