af37b2b59fdd910e53703f96743bf1668a6015c8
Oxshady <shadymahmod55@gmail.com>
2024-08-06 02:04:39 +0300

    done

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -30,7 +30,9 @@ def get_place(place_id):
     return jsonify(place.to_dict())
 
 
-@app_views.route("/places/<place_id>", methods=['DELETE'], strict_slashes=False)
+@app_views.route(
+    "/places/<place_id>",
+    methods=['DELETE'], strict_slashes=False)
 def delete_place(place_id):
     """delete specific place by city id endpoint"""
     place = storage.get(Place, place_id)
